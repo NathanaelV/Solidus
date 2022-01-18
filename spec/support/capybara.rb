9115294ca95abde6bef989e54a82b70f9241a1f4
NathanaelV <tmntsplinter04@gmail.com>
2022-01-18 09:18:52 -0300

    código do click_icon

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -8,3 +8,16 @@ RSpec.configure do |config|
   end
 end
 
+RSpec.describe 'The option types admin' do
+  it 'allows me to delete the option types' do
+    sign_in create(:admin_user)
+    option_type = create(:option_type)
+
+    visit spree.admin_path
+    click_link 'Products'
+    click_link 'Option Types'
+    click_icon 'trash'
+
+    expect(page).to have_content('has been successfully removed')
+  end
+end
