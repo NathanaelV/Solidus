75a5c863d3e3e26d61e7ab649aa309a091b9ae62
NathanaelV <tmntsplinter04@gmail.com>
2022-01-19 09:28:39 -0300

    Coloquei o código o order_walkthrough no factory_bot

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,3 +2,17 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
+RSpec.describe 'The checkout flow' do
+  it 'renders the delivery step' do
+    user = create(:user)
+    sign_in user
+    # Generate an order in the `delivery` state
+    order = Spree::TestingSupport::OrderWalkthrough.up_to(:address)
+    order.update!(user: user)
+
+    visit spree.checkout_path
+
+    expect(page).to have_current_path('/checkout/delivery')
+  end
+end
+
